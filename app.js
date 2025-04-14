9c4d310045bc22219059395b42ea0660be4deabf
akshitmudgal001 <akshitmudgal001@gmail.com>
2025-04-14 18:03:40 +0530

    minor fixes responsiveness

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/', webRouter);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack);
+  console.error(err.stack); 
   res.status(500).render('error', { title: 'Error' });
 });
 
